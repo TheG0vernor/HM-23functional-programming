d2367ad4671687487e0f70af41d9acb9b9f66c90
Вадим <niras.sp@gmail.com>
2022-09-19 16:14:08 +0700

    beta

--- app.py
+++ app.py
@@ -1,17 +1,80 @@
-import os
+from flask import Flask, abort, request
 
-from flask import Flask
+from constants import LOG_DIR
+from functions import filter_and_mapping, das_filter, das_mapping, unique_, limit, mapping_and_filter, sorted_
 
 app = Flask(__name__)
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
 
-
-@app.route("/perform_query")
+@app.route("/perform_query/", methods=['POST'])
 def perform_query():
     # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
     # вернуть пользователю сформированный результат
-    return app.response_class('', content_type="text/plain")
+    try:
+        cmd1 = request.args.get('cmd1')
+        value1 = request.args.get('value1')
+        cmd2 = request.args.get('cmd2')
+        value2 = request.args.get('value2')
+        file_name = request.args.get('file_name')
+        if file_name not in LOG_DIR:
+            abort(400)
+
+        # блок кода, где cmd1 == filter
+        elif cmd1 == 'filter' and cmd2 == 'map':
+            return filter_and_mapping(str_=value1, col=value2)
+        elif cmd1 == 'filter' and cmd2 == 'limit':
+            data = das_filter(str_=value1)
+            return limit(value=value2, data=data)
+        elif cmd1 == 'filter' and cmd2 == 'unique':
+            data = das_filter(str_=value1)
+            return unique_(data=data)
+        elif cmd1 == 'filter' and cmd2 == 'sort':
+            data = das_filter(str_=value1)
+            return sorted_(data=data, asc=value2)
+
+        # блок кода, где cmd1 == map
+        elif cmd1 == 'map' and cmd2 == 'unique':
+            data = das_mapping(col=value1)
+            return unique_(data=data)
+        elif cmd1 == 'map' and cmd2 == 'limit':
+            data = das_mapping(col=value1)
+            return limit(value=value2, data=data)
+        elif cmd1 == 'map' and cmd2 == 'filter':
+            return mapping_and_filter(col=value1, str_=value2)
+        elif cmd1 == 'map' and cmd2 == 'sort':
+            data = das_mapping(col=value1)
+            return sorted_(data=data, asc=value2)
+
+        # блок кода, где cmd1 == sort
+        elif cmd1 == 'sort' and cmd2 == 'limit':
+            data = sorted_(asc=value1)
+            return limit(value=value2, data=data)
+        elif cmd1 == 'sort' and cmd2 == 'filter':
+            data = sorted_(asc=value1)
+            return das_filter(str_=value2, data=data)
+        elif cmd1 == 'sort' and cmd2 == 'unique':
+            data = sorted_(asc=value1)
+            return unique_(data=data)
+        elif cmd1 == 'sort' and cmd2 == 'map':
+            data = sorted_(asc=value1)
+            return das_mapping(col=value2, data=data)
+
+        # блок кода, где cmd1 == limit
+        elif cmd1 == 'limit' and cmd2 == 'map':
+            data = limit(value=value1)
+            return das_mapping(col=value2, data=data)
+        elif cmd1 == 'limit' and cmd2 == 'filter':
+            data = limit(value=value1)
+            return das_filter(str_=value2, data=data)
+        elif cmd1 == 'limit' and cmd2 == 'sort':
+            data = limit(value=value1)
+            return sorted_(data=data, asc=value2)
+        elif cmd1 == 'limit' and cmd2 == 'unique':
+            data = limit(value=value1)
+            return unique_(data=data)
+        else:
+            abort(400)
+    except:
+        abort(400)
---
+++ functions.py
@@ -0,0 +1,70 @@
+from timeit import timeit
+
+from constants import LOG_DIR
+
+
+def das_mapping(col, data=None):
+    """Принимает номер колонки (может принять массив) и возвращает требуемую колонку из массива"""
+    if data is None:
+        data = das_data()
+    col = int(col)
+    result = map(lambda v: v.split(' ')[col], data)
+    return list(result)
+
+
+def mapping_and_filter(col, str_):
+    """Принимает номер колонки и значение и возвращает колонку строк с этим значением"""
+    col = int(col)
+    result = das_mapping(col)
+    result = das_filter(str_, result)
+    return list(result)
+
+
+def das_filter(str_, data=None):
+    """Принимает значение (может принять массив) и возвращает строки с этим значением"""
+    if data is None:
+        data = das_data()
+    result = filter(lambda v: v if str_ in v else None, data)
+    return list(result)
+
+
+def filter_and_mapping(str_, col):
+    """Принимает номер колонки и значение и возвращает колонку строк с этим значением"""
+    col = int(col)
+    result = das_filter(str_)
+    result = das_mapping(col, result)
+    return list(result)
+
+
+def unique_(data):
+    """Вернет массив с уникальными значениями"""
+    return list(set(data))
+
+
+def sorted_(asc, data=None):
+    """Сортирует массив"""
+    if data is None:
+        data = das_data()
+    if asc == 'asc':
+        return sorted(data)
+    elif asc == 'desc':
+        return sorted(data, reverse=True)
+
+
+def limit(value, data=None):
+    """Лимитирует вывод данных с массива"""
+    if data is None:
+        data = das_data()
+    value = int(value)
+    return data[:value]
+
+
+def das_data():
+    """Формирует массив с данными"""
+    with open(LOG_DIR) as f:
+        data = map(lambda v: v.strip(), f)  # используем итератор, чтобы не загружать в память весь массив с данными
+        return list(data)
+        # data = f.read()
+        # data = data.strip()
+        # data = data.split('\n')
+        # return data
